ddf0304feaa3ee5b3f3d0ce65a9e16e2eef9d807
Galactic Alex <Lehahahaigaleksus@mail.ru>
2024-02-26 17:16:35 +0300

    Prepared statement changes + sys variable pass

--- src/main/java/overridetech/jdbc/jpa/util/Util.java
+++ src/main/java/overridetech/jdbc/jpa/util/Util.java
@@ -9,7 +9,6 @@ public class Util {
     // реализуйте настройку соеденения с БД
     private final String jdbcURL = "jdbc:postgresql://localhost:5433/firstDB";
     private final String userName = "postgres";
-    private final String password = "879U63jPw54";
 
     public Util() {
     }
@@ -17,6 +16,7 @@ public class Util {
     public Optional<Connection> getOptionalConnection() {
         Optional<Connection> optional = Optional.empty();
         try {
+            String password = System.getenv("PGPASSWORD");
             optional = Optional.ofNullable(DriverManager.getConnection(jdbcURL, userName, password));
             return optional;
         } catch (SQLException e) {
